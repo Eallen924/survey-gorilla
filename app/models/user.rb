fe6c3ed8bbe863c7a8bd05f7bca92de2055a577d
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-05 15:51:08 -0500

    user auth and sessions added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ActiveRecord::Base
   validates :password, presence: true
   has_many :surveys
   
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(pass)
+    @entered_password = pass
+    @password = Password.create(pass)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)
+    return user if user && (user.password == password)
+    nil # either invalid email or wrong password
+  end
 end
