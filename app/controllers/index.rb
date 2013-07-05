50bec0b697764948161b08dae028898877ceb14c
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-05 15:48:30 -0500

    sign in, log-in, form views added

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,28 @@
+before do
+  @current_user = nil;
+end
+
 get '/' do
   # Look in app/views/index.erb
+  @current_user = nil;
   erb :index
+end
+
+get '/sign_in' do
+  # @user = {}
+  @email = 'an-email but not really;d'
+  erb :sign_in
+end
+
+get '/sign_up' do
+  # @user = {}
+  erb :sign_up
+end
+
+get '/list_surveys' do 
+  erb :list_surveys
+end
+
+get'/create_survey' do 
+  erb :create_survey
 end
